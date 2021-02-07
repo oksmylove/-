cd805f4fbecdb388177b4279753280eb5dfe4156
oksmylove <73633935+oksmylove@users.noreply.github.com>
2021-02-07 22:37:31 +0700

    Delete Task2.py

--- First/Task2.py
+++
@@ -1,6 +0,0 @@
-def input_(msg):
-    input_data = input(msg)
-    if not input_data.isdigit(): return input_("Вы ввели не число. Введите число: ")
-    if 10 <= int(input_data) <= 50: return input_data
-    return input_("Ваше число не диапазоне. Введите число:")
-print(input_("Введите число: "))
