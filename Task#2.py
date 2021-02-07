4a2e4c3f56f5dda8746f2ca329c80f98e0f369e5
oksmylove <73633935+oksmylove@users.noreply.github.com>
2021-02-08 00:37:44 +0700

    Add files via upload

---
+++ Task#2.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+import numpy as np
+from itertools import product, combinations
+from matplotlib.patches import FancyArrowPatch
+from mpl_toolkits.mplot3d import proj3d
+
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+ax.set_aspect("auto")
+
+#рисуем сферу
+u, v = np.mgrid[0:2*np.pi:20j, 0:np.pi:10j]
+x = np.cos(u)*np.sin(v)
+y = np.sin(u)*np.sin(v)
+z = np.cos(v)
+ax.plot_wireframe(x, y, z, color="r")
+
+class Arrow3D(FancyArrowPatch):
+
+    def __init__(self, xs, ys, zs, *args, **kwargs):
+        FancyArrowPatch.__init__(self, (0, 0), (0, 0), *args, **kwargs)
+        self._verts3d = xs, ys, zs
+
+    def draw(self, renderer):
+        xs3d, ys3d, zs3d = self._verts3d
+        xs, ys, zs = proj3d.proj_transform(xs3d, ys3d, zs3d, renderer.M)
+        self.set_positions((xs[0], ys[0]), (xs[1], ys[1]))
+        FancyArrowPatch.draw(self, renderer)
+
+a = Arrow3D([0.5, 1], [-1, 1], [-1, 1], mutation_scale=20,
+            lw=1, arrowstyle="-", color="k")
+ax.add_artist(a)
+
+plt.show()
+
+
+# In[ ]:
+
+
+
+
